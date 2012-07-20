f83b83e168ca6eba7fd2c036c4e6b834282f83fd
Ju Qian <jqian@nuaa.edu.cn>
2012-07-20 18:29:02 +0800

    change structure

--- ConRef/src/com/conref/sootUtil/callgraph/CallGraphEdgeFilter.java
+++
@@ -1,9 +0,0 @@
-package com.conref.sootUtil.callgraph;
-
-import soot.jimple.toolkits.callgraph.Edge;
-
-public class CallGraphEdgeFilter{
-	public boolean isIgnored(Edge e){
-		return false;
-	}
-}
--- ConRef/src/com/conref/sootUtil/callgraph/CallGraphNodeFilter.java
+++
@@ -1,9 +0,0 @@
-package com.conref.sootUtil.callgraph;
-
-import soot.MethodOrMethodContext;
-
-public class CallGraphNodeFilter {
-	public boolean isIngored(MethodOrMethodContext m){
-		return false;
-	}
-}
